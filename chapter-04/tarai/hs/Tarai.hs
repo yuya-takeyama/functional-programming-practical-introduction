21b09ab99705f1070546723a1550edeb2007a186
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2015-06-28 13:49:29 +0900

    Add chapter 04 tarai

---
+++ chapter-04/tarai/hs/Tarai.hs
@@ -0,0 +1,7 @@
+tarai :: Int -> Int -> Int -> Int
+tarai x y z
+  | x <= y    = y
+  | otherwise = tarai
+                (tarai (x - 1) y z)
+                (tarai (y - 1) z x)
+                (tarai (z - 1) x y)
